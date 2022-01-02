59c37ea9bc769cbdc4c4a159a4b85d0bd78f12f7
Gustav Oosthuizen <gustav@cogodata.co.za>
2022-01-02 19:46:56 +0200

    Documented all NN results for Network data set. Starting process for transac data set.

--- experiment_2.R
+++ experiment_2.R
@@ -1,18 +1,22 @@
-######## Experiment 1 ########
+######## Experiment 2 ########
 
-# Description/Goal of experiment: To investigate what are good hyper-parameters for units in hidden layer, batch size, and epochs. 
+# Description/Goal of experiment: To investigate what the effect would be if L2 regularization is applied.
 
 # 1. Defining Flags we are going to use
 
-FLAGS <- flags(flag_integer('dense_units', 8),
-               flag_integer('batch_size', 10),
-               flag_integer('epochs', 5)
-               )
+FLAGS <- flags(flag_integer('dense_units', 64),
+               flag_integer('batch_size', 5),
+               flag_integer('epochs', 67),
+               flag_numeric("lambda", 0.001)
+)
 
 # 2. Defining Model for experiment
 
 model <- keras_model_sequential() %>%
-  layer_dense(units = FLAGS$dense_units, activation = 'relu', input_shape = c(21)) %>%
+  layer_dense(units = FLAGS$dense_units, 
+              activation = 'relu', 
+              input_shape = c(21),
+              kernel_regularizer = regularizer_l2(l = FLAGS$lambda)) %>%
   layer_dense(units = 2, activation = 'softmax')
 
 # 3. Compile model
--- experiment_3.R
+++ experiment_3.R
@@ -1,18 +1,22 @@
-######## Experiment 1 ########
+######## Experiment 3 ########
 
-# Description/Goal of experiment: To investigate what are good hyper-parameters for units in hidden layer, batch size, and epochs. 
+# Description/Goal of experiment: To investigate what the effect would be if dropout regularization is applied.
 
 # 1. Defining Flags we are going to use
 
 FLAGS <- flags(flag_integer('dense_units', 8),
-               flag_integer('batch_size', 10),
-               flag_integer('epochs', 5)
-               )
+               flag_integer('batch_size', 80),
+               flag_integer('epochs', 16),
+               flag_numeric("dropout", 0.5)
+)
 
 # 2. Defining Model for experiment
 
 model <- keras_model_sequential() %>%
-  layer_dense(units = FLAGS$dense_units, activation = 'relu', input_shape = c(21)) %>%
+  layer_dense(units = FLAGS$dense_units, 
+              activation = 'relu', 
+              input_shape = c(27)) %>%
+  layer_dropout(rate = FLAGS$dropout) %>% 
   layer_dense(units = 2, activation = 'softmax')
 
 # 3. Compile model
@@ -24,7 +28,7 @@ model %>% compile(loss = 'categorical_crossentropy',
 # 4. Fit model
 
 history <- model %>% 
-  fit(train_transactional_features_nn,
+  fit(train_network_features_nn,
       train_target_nn,
       epochs = FLAGS$epochs,
       batch_size = FLAGS$batch_size,
